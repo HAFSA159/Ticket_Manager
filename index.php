a8d9931d5cf98b08e19c3704b92f65dbab02387b
HAFSA159 <hafsaelmoatassim99@gmail.com>
2023-12-17 21:31:10 +0100

    home page done

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Public/styles.scss">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<div class="noise"></div>
+<div class="overlay"></div>
+<div class="terminal">
+  <h1>Error <span class="errorcode">404</span></h1>
+  <p class="output">The destination you seek is on this page—savor your ticket.</p>
+  <p class="output">Please try to <a href="./Views/login.php">Take a ticket</a> or <a href="#2">return to the homepage</a>.</p>
+  <p class="output">Good luck.</p>
+</div>
+  <script defer src="./Public/style.js"></script>
+</body>
+</html>
